7ea1b9d8182a9261e0bbeb62bced46952defee32
ferdi1ansyah <ferdiferdian34@gmail.com>
2020-03-12 13:45:17 +0700

    first commit

---
+++ app/src/main/java/id/ac/polinema/recyclerview/ViewTypeActivity.java
@@ -0,0 +1,37 @@
+package id.ac.polinema.recyclerview;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import id.ac.polinema.recyclerview.adapters.ReversedLogoAdapter;
+import id.ac.polinema.recyclerview.models.TeamLogo;
+
+public class ViewTypeActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_type);
+
+		RecyclerView teamsView = findViewById(R.id.rv_teams);
+
+		List<TeamLogo> teams = new ArrayList<>();
+		teams.add(new TeamLogo("https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Liverpool_FC.svg/360px-Liverpool_FC.svg.png", "Liverpool"));
+		teams.add(new TeamLogo("https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/360px-Manchester_City_FC_badge.svg.png", "Man. City"));
+		teams.add(new TeamLogo("https://upload.wikimedia.org/wikipedia/id/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png", "Manchester United"));
+		teams.add(new TeamLogo("https://upload.wikimedia.org/wikipedia/id/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png", "FC Barcelona"));
+
+		ReversedLogoAdapter adapter = new ReversedLogoAdapter(this, teams);
+		teamsView.setAdapter(adapter);
+
+		RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+		teamsView.setLayoutManager(layoutManager);
+	}
+}
---
+++ app/src/main/java/id/ac/polinema/recyclerview/models/TeamLogo.java
@@ -0,0 +1,36 @@
+package id.ac.polinema.recyclerview.models;
+
+public class TeamLogo {
+
+
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+    public String getLogo() {
+        return logo;
+    }
+
+
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public TeamLogo(String logo, String nama) {
+        this.logo = logo;
+        this.nama = nama;
+    }
+
+    public String logo;
+
+    public String nama;
+
+
+}
+
+
